977747e74d60200a3a45c4ff6d85c549b652a8b6
Joseph NGOMA <jngoma@e4r19p2.wethinkcode.co.za>
2017-11-14 09:38:46 +0200

    Added cleaner to repo

---
+++ clean.sh
@@ -0,0 +1,8 @@
+find . -name "*.txt" -exec rm -f {} \;
+find . -name "*.swo" -exec rm -f {} \;
+find . -name "*.swp" -exec rm -f {} \;
+find . -name ".DS*" -exec rm -f {} \;
+find . -name "a.out" -exec rm -f {} \;
+find . -name "README*" -exec rm -f {} \;
+find . -name "clean.sh" -exec rm -f {} \;
+find . -name "*.pdf" -exec rm -f {} \;
