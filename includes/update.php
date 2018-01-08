681a5df42b858c7906dbfaaaaeee1a04f3508383
Joseph NGOMA <jngoma@e4r22p1.wethinkcode.co.za>
2018-01-08 14:14:14 +0200

    Backend for updating profile

---
+++ includes/update.php
@@ -0,0 +1,13 @@
+<div class="RegForm">
+  <div>
+    <h2>Complete your account</h2>
+    <form action="config/fillup.php" method="POST">
+      <label for="">Username</label>
+      <br>
+      <input type="text" name="puser_name" placeholder="Username"/>
+      <br>
+      <br>
+      <button type="submit" name="submit">Register</button><br/>
+    </form>
+  </div>
+</div>
