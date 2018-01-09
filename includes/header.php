dd94874b85e41561d23f0a194eac5d2061682f97
Joseph NGOMA <jngoma@e4r22p1.wethinkcode.co.za>
2018-01-09 11:06:45 +0200

    Home changes based on if your logged in or not

--- includes/header.php
+++ includes/header.php
@@ -3,17 +3,19 @@
     <h1>Matcha</h1>
   </div>
   <div class="NavBar">
-    <a href="index.php">Home</a>
+
     <?php
       if (!isset($_SESSION['username']))
       {
+        echo ("<a href='index.php'>Home </a>");
         echo ("<a href='login.php'>Login </a>");
         echo ("<a href='#'>Preview Site </a>");
       }
       if (isset($_SESSION['username']))
       {
+        echo ("<a href='cam.php'>Home </a>");
         echo ("<a href='profile.php'>Profile </a>");
-        echo ("<a href='#'>Logout </a>");
+        echo ("<a href='config/logout.php'>Logout </a>");
       }
     ?>
   </div>
