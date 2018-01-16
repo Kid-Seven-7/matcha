c61c335dca4eabf8348830b173f94b9b74ab0cff
Joseph NGOMA <jngoma@e4r22p1.wethinkcode.co.za>
2018-01-16 11:37:14 +0200

    globals->master classes->slave

--- includes/meta.php
+++ includes/meta.php
@@ -1,6 +1,8 @@
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<link rel="stylesheet" href="css/globals.css">
-<link rel="stylesheet" href="css/classes.css">
+<link rel="stylesheet" href="css/master.css">
+<link rel="stylesheet" href="css/slave.css">
 <link rel="stylesheet" href="css/media.css">
 <link rel="shortcut icon" href="http://pngimg.com/uploads/heart/heart_PNG679.png">
+<meta http-equiv="X-UA-Compatible" content="ie=edge">
+<script src="https://unpkg.com/axios/dist/axios.min.js"></script>
