cffec6652ee74c9cfb520487656e456ead9b0ac9
Joseph NGOMA <jngoma@e4r22p1.wethinkcode.co.za>
2018-01-16 11:35:43 +0200

    Basic search structure

---
+++ includes/search.php
@@ -0,0 +1,48 @@
+
+    <?php include_once 'includes/header.php' ?>
+    <?php
+    include_once('database.php');
+
+    echo "<div class='searchTable'>";
+    echo "<table style='border: solid 1px black;'>";
+     echo "<tr><th>Id</th><th>user_name</th><th>firstname</th><th>surname</th><th>email</th></tr>";
+
+    class TableRows extends RecursiveIteratorIterator {
+        function __construct($it) {
+            parent::__construct($it, self::LEAVES_ONLY);
+        }
+
+        function current() {
+            return "<td style='width: 150px; border: 1px solid black;'>" . parent::current(). "</td>";
+        }
+
+        function beginChildren() {
+            echo "<tr>";
+        }
+
+        function endChildren() {
+            echo "</tr>" . "\n";
+        }
+    }
+
+    try {
+        $conn = new PDO('mysql:host=127.0.0.1;dbname=Matcha', 'root', 'joseph07');
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $stmt = $conn->prepare("SELECT id, user_name, first_name, surname, email FROM users WHERE ");
+        $stmt->execute();
+
+        // set the resulting array to associative
+        $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
+
+        foreach(new TableRows(new RecursiveArrayIterator($stmt->fetchAll())) as $k=>$v) {
+          echo $v;
+        }
+        // die("hello world!");
+    }
+    catch(PDOException $e) {
+        echo "Error: " . $e->getMessage();
+    }
+    $conn = null;
+    echo "</table>";
+    echo "</div>";
+    ?>
