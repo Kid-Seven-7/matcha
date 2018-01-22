580f829f23aef6691b9a33eff51764cca2a5f123
Kid-Seven-7 <joseph.ngoma0707@gmail.com>
2018-01-22 09:19:48 +0200

    test function

---
+++ test.php
@@ -0,0 +1,23 @@
+<?php
+
+function update($field, $value, $email) {
+try {
+  $conn = new PDO('mysql:dbname=Matcha;host:127.0.0.1', 'root', 'joseph07');
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+  $sql = "UPDATE users
+          SET $field='$value'
+          WHERE email='$email'";
+  $stmt = $conn->prepare($sql);
+  $stmt->execute();
+}
+catch(PDOException $e) {
+  echo "error: ".$e;
+}
+
+ ?>
+
+
+if (this){
+  update(surname, $surname, $email)
+}
