b4763a42b2e1e8431cff47f38641c3fb15d079f6
Joseph NGOMA <jngoma@e4r19p3.wethinkcode.co.za>
2018-02-27 16:12:50 +0200

    Message sending function

---
+++ includes/sendmsg.php
@@ -0,0 +1,36 @@
+<?php
+session_start();
+
+if (isset($_POST['submit'])){
+  $msg = $_POST['message'];
+  $send_to = $_POST['send_to'];
+  $conn_id = $_POST['conn_id'];
+  $sent_by = $_SESSION['username'];
+
+  if (empty($msg)){
+    header("Location: ../chat.php?msg=empty");
+  }else if(empty($conn_id)){
+    header("Location: ../chat.php?conn_id=empty");
+  }
+
+  try{
+    $conn = new PDO('mysql:host=127.0.0.1;dbname=Matcha', 'root', 'joseph07');
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+
+    $stmt = $conn->prepare("INSERT INTO chats (sent_to, sent_by, message, connection_id)
+                            VALUES (:send_to, :sent_by, :message, :conn_id)");
+    $stmt->execute(array(':send_to' => $send_to,
+                          ':sent_by' => $sent_by,
+                          ':message' => $msg,
+                          ':conn_id' => $conn_id));
+  }catch(PDOException $e){
+    echo "error: {$e}";
+  }
+
+}
+
+header("Location: ../chat.php?conn_id={$conn_id}")
+
+
+?>
