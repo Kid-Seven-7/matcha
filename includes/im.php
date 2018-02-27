7d02268e41c2dec9ef8b1fdf3b443dbec95cabb4
Joseph NGOMA <jngoma@e4r19p3.wethinkcode.co.za>
2018-02-27 16:11:07 +0200

    Functional chat and view profile section. Needs Ajax

--- includes/im.php
+++ includes/im.php
@@ -1,25 +1,74 @@
 <?php
+session_start();
 
-try {
-  $i = 0;
-  //Inserting data to the database
-  $conn = new PDO('mysql:host=127.0.0.1;dbname=Matcha', 'root', 'joseph07');
-  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+if ($_GET['conn_id']){
+  try {
+    //Inserting data to the database
+    $conn = new PDO('mysql:host=127.0.0.1;dbname=Matcha', 'root', 'joseph07');
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 
-  $stmt = $conn->prepare("SELECT *
-                          FROM chats
-                          WHERE sent_to = :user
-                          AND opened = 0");
-  $stmt->execute(array(':user' => $_SESSION['username']));
-  $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
-  foreach($result as $row) {
-    echo "{$row[sent_by]}:<br> {$row[message]}<br><textarea placeholder='reply' rows='7' cols='30'></textarea>";
-    $i++;
+    $stmt = $conn->prepare("SELECT *
+                            FROM chats
+                            WHERE connection_id = :conn_id");
+    $stmt->execute(array(':conn_id' => $_GET['conn_id']));
+    $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    foreach($result as $row) {
+      $send_to = null;
+      $i = 0;
+      if($row[sent_by] == $_SESSION['username']){
+        $send_to = $row[sent_to];
+      }else{
+        $send_to = $row[sent_by];
+      }
+      if ($row[sent_by] == $_SESSION['username']){
+        echo "<div class='msgBoxSent'><strong>Me:</strong><br> {$row[message]}</div><br>";
+      }else{
+        echo "<div class='msgBoxReceived'><strong>{$row[sent_by]}:</strong><br> {$row[message]}</div><br>";
+      }
+      $i++;
+    }
+    if ($i == 0){
+      echo "<div class='msgBoxSent'><strong>Match Bot:</strong><br> You have no messages at present :(</div><br>";
+    }
+    echo "<form action='includes/sendmsg.php' method='POST'>
+      <textarea class='replyBox' name='message' rows='8' cols='80' placeholder='reply'></textarea><br>
+      <input type='hidden' name='conn_id' value='{$_GET[conn_id]}'>
+      <input type='hidden' name='send_to' value='{$send_to}'>
+      <input class='inputButton' type='submit' name='submit' value='reply'>
+    </form>";
+
+  }catch(PDOException $e) {
+    echo "error: ".$e;
   }
+}elseif ($_GET['view']) {
+  try {
+    $user = $_GET['view'];
+    //Inserting data to the database
+    $conn = new PDO('mysql:host=127.0.0.1;dbname=Matcha', 'root', 'joseph07');
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-}catch(PDOException $e) {
-  echo "error: ".$e;
+
+    $stmt = $conn->prepare("SELECT *
+                            FROM users
+                            WHERE user_name = :user_name");
+    $stmt->execute(array(':user_name' => $user));
+    $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    foreach($result as $row) {
+      echo "<div class='suggestions' style='float: left; width:25%'>";
+      echo "<a href='checkout.php?user={$row['id']}'><img src='uploads/{$row['profilePic']}'></a>";
+      echo "</div>";
+      echo "<div style='float:right; width:75%'>";
+      echo "<strong>Username:</strong> {$row['user_name']}<br>";
+      echo "<strong>Full name:</strong> {$row['first_name']} {$row['surname']}<br>";
+      echo "<strong>Age:</strong> {$row['age']}<br>";
+      echo "<strong>Bio:</strong> {$row['bio']}<br>";
+      echo "<strong>Last seen:</strong> {$row['lastseen']}<br>";
+      echo "</div>";
+    }
+  }catch(PDOException $e) {
+    echo "error: ".$e;
+  }
 }
 
 ?>
