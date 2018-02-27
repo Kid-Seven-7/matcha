4366ef467ecd06490d71c08c9734d43ea9a10ff7
Joseph NGOMA <jngoma@e4r19p3.wethinkcode.co.za>
2018-02-27 18:12:12 +0200

    working on the opened part of chats

--- includes/im.php
+++ includes/im.php
@@ -41,6 +41,20 @@ if ($_GET['conn_id']){
   }catch(PDOException $e) {
     echo "error: ".$e;
   }
+
+  // try{
+  //   $conn = new PDO('mysql:host=127.0.0.1;dbname=Matcha', 'root', 'joseph07');
+  //   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  //
+  //
+  //   $stmt = $conn->prepare("UPDATE *
+  //                           FROM chats
+  //                           WHERE connection_id = :conn_id");
+  //   $stmt->execute(array(':conn_id' => $_GET['conn_id']));
+  // }catch(PDOException $e){
+  //   echo "error: ".$e;
+  // }
+
 }elseif ($_GET['view']) {
   try {
     $user = $_GET['view'];
@@ -49,23 +63,12 @@ if ($_GET['conn_id']){
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 
-    $stmt = $conn->prepare("SELECT *
-                            FROM users
-                            WHERE user_name = :user_name");
+    $stmt = $conn->prepare("SELECT  chats
+                            SET seen = 1
+                            WHERE send_to = :user_name
+                            AND sent_by = :username");
     $stmt->execute(array(':user_name' => $user));
-    $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
-    foreach($result as $row) {
-      echo "<div class='suggestions' style='float: left; width:25%'>";
-      echo "<a href='checkout.php?user={$row['id']}'><img src='uploads/{$row['profilePic']}'></a>";
-      echo "</div>";
-      echo "<div style='float:right; width:75%'>";
-      echo "<strong>Username:</strong> {$row['user_name']}<br>";
-      echo "<strong>Full name:</strong> {$row['first_name']} {$row['surname']}<br>";
-      echo "<strong>Age:</strong> {$row['age']}<br>";
-      echo "<strong>Bio:</strong> {$row['bio']}<br>";
-      echo "<strong>Last seen:</strong> {$row['lastseen']}<br>";
-      echo "</div>";
-    }
+
   }catch(PDOException $e) {
     echo "error: ".$e;
   }
