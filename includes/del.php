2e2ebd39a616b6fd8a6c5fe1e79fffa2993ce82a
amatshiye <amatshiy@student.42.fr>
2018-04-12 20:08:03 +0200

    added database include

--- includes/del.php
+++ includes/del.php
@@ -1,9 +1,12 @@
 <?php
 session_start();
+require_once('../config/database.php');
+
 $email = $_SESSION['email'];
 
 try {
-  $conn = new PDO('mysql:dbname=Matcha;host:127.0.0.1', 'root', 'joseph07');
+  $conn = new PDO($DB_DSN, $DB_USER, $DB_PASSWORD);
+  // $conn = new PDO('mysql:dbname=Matcha;host:127.0.0.1', 'root', 'joseph07');
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
   if($_GET['rem'] == 'tattoos'){
     $sql = "UPDATE users
