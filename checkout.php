db1717a0958a137f198c248c428465e2bab5052f
amatshiye <amatshiy@student.42.fr>
2018-04-26 21:50:45 +0200

    removed empty lines

--- checkout.php
+++ checkout.php
@@ -18,8 +18,6 @@ if(isset($_GET['user'])){
     <div class="checkingOut">
       <?php
 
-
-
       try {
         $conn = new PDO($DB_DSN, $DB_USER, $DB_PASSWORD);
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
